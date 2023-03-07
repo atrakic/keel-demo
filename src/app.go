2d007a62b987d6080d8b2c90980997498d34d0a7
Admir Trakic <atrakic@users.noreply.github.com>
2023-03-07 19:10:41 +0100

    feat: add /healhz path

--- src/app.go
+++ src/app.go
@@ -9,9 +9,17 @@ import (
 var version = "v0.0.1"
 
 func main() {
+
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("server", "keel-demo")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "OK\n")
+	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website! Version %s", version)
 	})
+
 	fmt.Printf("App is starting, version: %s \n", version)
 	log.Fatal(http.ListenAndServe(":8500", nil))
 }
